2db5b04929d270ca7f0d46bb1b4fe0f266ca9c75
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-10-29 22:55:03 +0900

    useCheckItem test

---
+++ src/hooks/test/useCheckItem.test.tsx
@@ -0,0 +1,57 @@
+import { renderHook, act } from '@testing-library/react';
+import useCheckItem from '../useCheckItem';
+import useRenderItem from '../useRenderItem';
+
+jest.mock('../useRenderItem');
+
+describe('useCheckItem test', () => {
+    let mockGetDatas: jest.Mock;
+
+    beforeEach(() => {
+        mockGetDatas = jest.fn();
+        (useRenderItem as jest.Mock).mockReturnValue({ getDatas: mockGetDatas });
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('check todoItem on sucessful fetch', async () => {
+        global.fetch = jest.fn(() => 
+            Promise.resolve({
+                ok: true
+            })
+        ) as jest.Mock;
+
+        const { result } = renderHook(() => useCheckItem());
+
+        await act(async() => {
+            await result.current.handleCheckTodo(1);
+        });
+
+        expect(fetch).toHaveBeenCalledWith('/api/todos/check/1', {
+            method: 'PATCH', 
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        expect(mockGetDatas).toHaveBeenCalled();
+    });
+
+    test('alert on failed fetch', async () => {
+        global.fetch = jest.fn(() => 
+            Promise.resolve({
+                ok: false
+            })
+        ) as jest.Mock;
+
+        global.alert = jest.fn();
+
+        const { result } = renderHook(() => useCheckItem());
+
+        await act(async() => {
+            await result.current.handleCheckTodo(1);
+        });
+
+        expect(alert).toHaveBeenCalledWith('useCheckItem.ts 에러');
+    });
+});
